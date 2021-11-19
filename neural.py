67d116f44d2d54f37757452fbbf88bf2a9a2da26
NEO <cwitenice@gmail.com>
2021-11-19 22:56:19 +0000

    halfway thru

--- neural.py
+++ neural.py
@@ -3,14 +3,16 @@ inputs = [1,2,3,2.5]
 
 
 
-weights = [[0.2,0.8,-0.5,1.0], [0.5,-0.91,0.26,-0.5], [-0.26,-0.27,0.17,0.87]]
+weights = [[0.2,0.8,-0.5,1.0], 
+           [0.5,-0.91,0.26,-0.5], 
+           [-0.26,-0.27,0.17,0.87]]
 
 biases = [2,3,0.5]
 
 layer_outputs = []
 
 
-# zip() adds two list together
+# zip() adds two lists together
 
 for neuron_weights, neuron_bias in zip(weights, biases):
     neuron_output = 0
