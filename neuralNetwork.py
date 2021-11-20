74a00123f1abc09a6a8fcb1dc97a8419a8d1bb2c
morpheus <cwitenice@gmail.com>
2021-11-20 18:20:28 +0000

    Transpose Weights (switch rows and coloums)

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-# vector
-inputs = [1,2,3,2.5]
-# matrix (morpheus)
+# matrix
+inputs = [[1,2,3,2.5],
+          [2.0,5.0,-1.0,2.0],
+          [-1.5,2.7,3.3,-0.8]]
+
+# matrix 
 weights = [[0.2,0.8,-0.5,1.0], 
            [0.5,-0.91,0.26,-0.5], 
            [-0.26,-0.27,0.17,0.87]]
+
 biases = [2,3,0.5]
 
-output = np.dot(weights, inputs) + biases
-print(output)
+output = np.dot(inputs, np.array(weights).T) + biases
+print(output) 
