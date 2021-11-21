d91b833edd32a0241973ee03700f77315b91f659
morpheus <cwitenice@gmail.com>
2021-11-21 21:44:47 +0000

    8

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,15 +4,6 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-# matrix
-# common practice to name inputs as X
-X = [[1,2,3,2.5],
-    [2.0,5.0,-1.0,2.0],
-    [-1.5,2.7,3.3,-0.8]]
-
-X, y = spiral_data(100,3)
-
-
 
 #n oop
 class Layer_Dense:
@@ -23,19 +14,56 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
         
 
-
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
         
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output  = probabilities
         
         
-# layer_Dense( numbers of the rows in inputs,  how many neurons you want )
-layer1 = Layer_Dense(2,5)
-activation1 = Activation_ReLU()
-layer1.forward(X)
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
 
-# print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+        
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+            
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+            
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods    
+        
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss: ", loss)
